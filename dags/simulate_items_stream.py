44a063e11855ccec704b1936f4cf821afec7c8ae
Eduardo <almazan94@gmail.com>
2025-03-27 17:09:46 +0100

    update stream dag

---
+++ dags/simulate_items_stream.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+from utils.pubsub_utils import simulate_streaming_from_csv
+
+default_args = {
+    'start_date': datetime(2025, 1, 1),
+}
+
+with DAG("simulate_stream_items",
+         schedule_interval=None,
+         catchup=False,
+         default_args=default_args,
+         tags=["streaming"],
+         description="Simulate streaming of raw_items.csv to Pub/Sub") as dag:
+
+    simulate_task = PythonOperator(
+        task_id="simulate_items_stream",
+        python_callable=simulate_streaming_from_csv,
+        op_kwargs={
+            "url": "https://raw.githubusercontent.com/EAlmazanG/gcp-dbt/8e49f9268c9f82fdc6348294ddbcb26269d571cb/data/raw_items.csv",
+            "topic_path": "projects/gcp-dbt-454911/topics/items-stream",
+            "batch_size": 1000,
+            "interval_secs": 1
+        }
+    )
