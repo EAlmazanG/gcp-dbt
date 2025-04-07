025c37897f86e2a8a938f0f848843b0b4c79e55a
Eduardo <almazan94@gmail.com>
2025-04-07 09:54:46 +0200

    add cast

--- dbt/gcp_dbt_project/models/staging/raw_batch/base/base_raw_batch__customers.sql
+++ dbt/gcp_dbt_project/models/staging/raw_batch/base/base_raw_batch__customers.sql
@@ -5,6 +5,6 @@
 with source as (select * from {{ source('raw_batch', 'customers') }})
 
 select
-    string_field_0 as customer_id,
-    string_field_1 as customer_name
+    cast(string_field_0 as string) as customer_id,
+    cast(string_field_1 as string) as customer_name
 from source
