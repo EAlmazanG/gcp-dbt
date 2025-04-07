0f7fbfa00ec3e6bb403771ae925cbb5524c68da5
Eduardo <almazan94@gmail.com>
2025-04-07 11:40:53 +0200

    reorder

--- dbt/gcp_dbt_project/models/staging/raw_streaming/base/base_raw_streaming__items.sql
+++ dbt/gcp_dbt_project/models/staging/raw_streaming/base/base_raw_streaming__items.sql
@@ -5,8 +5,8 @@
 with source as (select * from {{ source('raw_streaming', 'items') }})
 
 select
-  timestamp_millis(timestamp) AS pubsub_event_timestamp,
   json_value(cast(message as string), '$.id') as item_id,
   json_value(cast(message as string), '$.order_id') as order_id,
-  json_value(cast(message as string), '$.sku') as product_id
+  json_value(cast(message as string), '$.sku') as product_id,
+  timestamp_millis(timestamp) AS pubsub_event_timestamp
 from source
--- dbt/gcp_dbt_project/models/staging/raw_streaming/base/base_raw_streaming__orders.sql
+++ dbt/gcp_dbt_project/models/staging/raw_streaming/base/base_raw_streaming__orders.sql
@@ -5,12 +5,12 @@
 with source as (select *, cast(message as string) as message_json from {{ source('raw_streaming', 'orders') }})
 
 select
-  timestamp_millis(timestamp) as pubsub_event_timestamp,
   json_value(message_json, '$.id') as order_id,
   json_value(message_json, '$.store_id') as store_id,
   json_value(message_json, '$.customer') as customer_id,
   parse_timestamp('%FT%T', json_value(message_json, '$.ordered_at')) as ordered_at,
   cast(cast(json_value(message_json, '$.subtotal') as float64) / 100 as float64) as order_subtotal_eur,
   cast(cast(json_value(message_json, '$.tax_paid') as float64) / 100 as float64) as order_tax_paid_eur,
-  cast(cast(json_value(message_json, '$.order_total') as float64) / 100 as float64) as order_total_eur
+  cast(cast(json_value(message_json, '$.order_total') as float64) / 100 as float64) as order_total_eur,
+  timestamp_millis(timestamp) as pubsub_event_timestamp
 from source
