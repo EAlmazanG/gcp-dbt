d020499f2c446d60eec3d109f642e828f9da817c
Eduardo <almazan94@gmail.com>
2025-04-07 11:31:26 +0200

    change order

--- dbt/gcp_dbt_project/models/staging/raw_batch/base/base_raw_batch__supplies.sql
+++ dbt/gcp_dbt_project/models/staging/raw_batch/base/base_raw_batch__supplies.sql
@@ -5,9 +5,9 @@
 with source as (select * from {{ source('raw_batch', 'supplies') }})
 
 select
+    {{ dbt_utils.generate_surrogate_key(['id', 'sku']) }} as supply_uuid,
     cast(id as string) as supply_id,
     cast(sku as string) as product_id,
-    {{ dbt_utils.generate_surrogate_key(['id', 'sku']) }} as supply_uuid,
     cast(name as string) as supply_name,
     cast(cost/100 as int64) as supply_cost_eur,
     perishable as is_perishable_supply
