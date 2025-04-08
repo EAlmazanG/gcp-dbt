1f06e3245054b792458dcb06ed33203ff4a70080
Eduardo <almazan94@gmail.com>
2025-04-08 12:06:52 +0200

    add rounds

--- dbt/gcp_dbt_project/models/staging/raw_batch/base/base_raw_batch__supplies.sql
+++ dbt/gcp_dbt_project/models/staging/raw_batch/base/base_raw_batch__supplies.sql
@@ -9,6 +9,6 @@ select
     cast(id as string) as supply_id,
     cast(sku as string) as product_id,
     cast(name as string) as supply_name,
-    cast(cost/100 as float64) as supply_cost_eur,
+    round(cast(cost/100 as float64), 2) as supply_cost_eur,
     perishable as is_perishable_supply
 from source
--- dbt/gcp_dbt_project/models/staging/raw_streaming/base/base_raw_streaming__orders.sql
+++ dbt/gcp_dbt_project/models/staging/raw_streaming/base/base_raw_streaming__orders.sql
@@ -9,8 +9,8 @@ select
   json_value(message_json, '$.store_id') as store_id,
   json_value(message_json, '$.customer') as customer_id,
   parse_timestamp('%FT%T', json_value(message_json, '$.ordered_at')) as ordered_at,
-  cast(cast(json_value(message_json, '$.subtotal') as float64) / 100 as float64) as order_subtotal_eur,
-  cast(cast(json_value(message_json, '$.tax_paid') as float64) / 100 as float64) as order_tax_paid_eur,
-  cast(cast(json_value(message_json, '$.order_total') as float64) / 100 as float64) as order_total_eur,
+  round(cast(cast(json_value(message_json, '$.subtotal') as float64) / 100 as float64), 2) as order_subtotal_eur,
+  round(cast(cast(json_value(message_json, '$.tax_paid') as float64) / 100 as float64), 2) as order_tax_paid_eur,
+  round(cast(cast(json_value(message_json, '$.order_total') as float64) / 100 as float64), 2) as order_total_eur,
   timestamp_millis(timestamp) as pubsub_event_timestamp
 from source
