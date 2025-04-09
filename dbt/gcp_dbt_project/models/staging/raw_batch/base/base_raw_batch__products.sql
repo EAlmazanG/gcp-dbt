4084d94f96c01fa411582fe915b3f6f172693c49
Eduardo <almazan94@gmail.com>
2025-04-09 10:54:02 +0200

    add type

--- dbt/gcp_dbt_project/models/staging/raw_batch/base/base_raw_batch__products.sql
+++ dbt/gcp_dbt_project/models/staging/raw_batch/base/base_raw_batch__products.sql
@@ -6,7 +6,10 @@ with source as (select * from {{ source('raw_batch', 'products') }})
 
 select
     cast(sku as string) as product_id,
+    cast(type as string) as product_type,
     cast(name as string) as product_name,
     cast(description as string) as product_description,
-    round(cast(price / 100 as float64), 2) as product_price_eur
+    round(cast(price / 100 as float64), 2) as product_price_eur,
+    ifnull(type = 'jaffle', false) as is_food_item,
+    ifnull(type = 'beverage', false) as is_drink_item
 from source
