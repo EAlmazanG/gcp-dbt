0a55ddd8add119abecc09c7ff4e2670631860ff4
Eduardo <almazan94@gmail.com>
2025-04-14 11:23:30 +0200

    add complete test dag

--- dags/run_complete_pipeline.py
+++ dags/run_complete_pipeline.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.utils.task_group import TaskGroup
 from datetime import datetime
 
 default_args = {
@@ -10,25 +11,65 @@ with DAG("run_complete_pipeline",
          schedule_interval=None,
          catchup=False,
          default_args=default_args,
-         tags=["orchestrator"],
-         description="Trigger all batch DAGs") as dag:
+         tags=["orchestration"],
+         description="Orquesta toda la pipeline de extremo a extremo") as dag:
 
-    trigger_customers = TriggerDagRunOperator(
-        task_id="trigger_customers",
-        trigger_dag_id="ingest_customers_batch"
+    with TaskGroup("stream_simulation") as stream_simulation:
+        simulate_items = TriggerDagRunOperator(
+            task_id="trigger_simulate_items_stream",
+            trigger_dag_id="simulate_items_stream",
+            wait_for_completion=True,
+            reset_dag_run=True
+        )
+
+        simulate_orders = TriggerDagRunOperator(
+            task_id="trigger_simulate_orders_stream",
+            trigger_dag_id="simulate_orders_stream",
+            wait_for_completion=True,
+            reset_dag_run=True
+        )
+
+    with TaskGroup("batch_ingest") as batch_ingest:
+        ingest_customers = TriggerDagRunOperator(
+            task_id="trigger_ingest_customers_batch",
+            trigger_dag_id="ingest_customers_batch",
+            wait_for_completion=True,
+            reset_dag_run=True
+        )
+
+        ingest_products = TriggerDagRunOperator(
+            task_id="trigger_ingest_products_batch",
+            trigger_dag_id="ingest_products_batch",
+            wait_for_completion=True,
+            reset_dag_run=True
+        )
+
+        ingest_stores = TriggerDagRunOperator(
+            task_id="trigger_ingest_stores_batch",
+            trigger_dag_id="ingest_stores_batch",
+            wait_for_completion=True,
+            reset_dag_run=True
+        )
+
+        ingest_supplies = TriggerDagRunOperator(
+            task_id="trigger_ingest_supplies_batch",
+            trigger_dag_id="ingest_supplies_batch",
+            wait_for_completion=True,
+            reset_dag_run=True
+        )
+
+    sync_gcs = TriggerDagRunOperator(
+        task_id="trigger_sync_gcs_raw_to_bq",
+        trigger_dag_id="sync_gcs_raw_to_bq",
+        wait_for_completion=True,
+        reset_dag_run=True
     )
 
-    trigger_products = TriggerDagRunOperator(
-        task_id="trigger_products",
-        trigger_dag_id="ingest_products_batch"
+    run_dbt = TriggerDagRunOperator(
+        task_id="trigger_launch_cloud_run_dbt_job",
+        trigger_dag_id="launch_cloud_run_dbt_job",
+        wait_for_completion=True,
+        reset_dag_run=True
     )
 
-    trigger_orders = TriggerDagRunOperator(
-        task_id="trigger_orders",
-        trigger_dag_id="ingest_orders_batch"
-    )
-
-    trigger_payments = TriggerDagRunOperator(
-        task_id="trigger_payments",
-        trigger_dag_id="ingest_payments_batch"
-    )
+    stream_simulation >> batch_ingest >> sync_gcs >> run_dbt
